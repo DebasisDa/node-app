fe97bf37ad043eb218bec7792784173a488d18b2
Tridibesh Nag <tridibesh.nag@aponiar.com>
2020-03-20 17:06:42 +0530

    issue resolve

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
  console.log('hello ghgjwohgrlffdcc.dd.');
- res.send('hello world 5');
+ res.send('hello world 10');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
